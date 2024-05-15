ca7b61310c8d4beeb6d1499c01b158715ea9cf23
Krystan K <Korn0018@algonquinlive.com>
2024-05-15 15:00:00 -0400

    Added Java Code

---
+++ src/User.java
@@ -0,0 +1,10 @@
+/**
+ * Date: Wednesday, May 15, 2024;
+ * @version 11.0.8.10.1
+ * @since 11.0.8
+ * @author Krystan K
+ * <p> Description: This class is used to create a template for all the users who will enter data into the system. Using accessors and mutators (setters and getters), the required information can be stored and retrieved as required for each User object.
+ */
+public class User {
+	
+}
